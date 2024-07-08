4cc5af911f05b9e86b53079eae1863c7bcb149d0
Myles181 <cyrile450@gmail.com>
2024-07-08 07:42:39 -0700

    more tests

--- stage-2/auth/app/tests/test.py
+++ stage-2/auth/app/tests/test.py
@@ -8,8 +8,8 @@ from ..models import Organisation, User
 class MyAuth(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.register_url = 'http://127.0.0.1:8000/auth/register'
-        self.login_url = 'http://127.0.0.1:8000/auth/login'
+        self.register_url = 'https://hng-intership.onrender.com/auth/register'
+        self.login_url = 'https://hng-intership.onrender.com/auth/login'
 
         self.payload = {
             "firstName": "Micheal",
@@ -32,9 +32,28 @@ class MyAuth(unittest.TestCase):
         response = requests.post(self.register_url, data=self.payload)
         data = response.json()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        organisation = Organisation.objects.get(orgId=data['data']['userId'])
-        print(organisation)
-        # self.assertAlmostEqual()
+        organisation = Organisation.objects.get(orgId=data['data']['user']['userId'])
+        self.assertEqual(organisation.name, f"{self.payload['firstName']}'s Organisation")
+
+    def test_login(self):
+        # Register new user
+        requests.post(self.register_url, data=self.payload)
+        # Login as new user
+        response = requests.post(self.login_url, data={"email": self.payload['email'], "password": self.payload['password']})
+        response_data = response.json()
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        access_token = response_data['data']['accessToken']
+        
+        # Access a protected route
+        userId = response_data['data']['user']['userId']
+        print(f"userId: {userId}")
+        getuser_url = f'https://hng-intership.onrender.com/users/{userId}'
+
+        user_response = requests.get(getuser_url, headers={"Content-Type": "application/json", "Authorization": f"Bearer {access_token}"})
+        self.assertEqual(user_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(user_response.json().status, "success")
+        print(user_response.json())
 
 
 if __name__ == "__main__":
