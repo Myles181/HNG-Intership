730117d4ae6b69cb616cb41f1f3fa9d66e93a38b
Emmanuel Cyril <cyrile450@gmail.com>
2024-07-16 13:46:24 -0700

    Update app.py

--- trenches/app.py
+++ trenches/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route("/apology")
 def sorry():
      return {
-        "message": "I sincerely apologize for violating the rules by posting a foreign link in the hng space. This is a result of my ignorance of not going through the rules and I promise will never happen again.",
-        "recipient": "Shully"
-        }
+          "message": "Dear Shully, I sincerely apologize for violating the rules by posting a foreign link in the HNG space. I realize this mistake was due to my failure to thoroughly review the guidelines. I assure you that this will not happen again, and I will be more diligent in adhering to the rules moving forward. Thank you for your understanding.",
+          "recipient": "Shully"
+     }
+
 
 if __name__ == "__main__":
      app.run()
